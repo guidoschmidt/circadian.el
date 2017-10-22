f587125845471adda75b43a22ee2b7289f3d34d8
Guido Schmidt <git@guidoschmidt.cc>
2017-10-22 13:16:17 +0200

    ADD/circadian.el-test.el: test sunrise/sunset configuration

--- test/circadian.el-test.el
+++ test/circadian.el-test.el
@@ -4,24 +4,27 @@
 ;;; Code:
 (require 'cl)
 (require 'el-mock)
+
 (load (expand-file-name "circadian.el" default-directory))
 
 
+
 (ert-deftest test-circadian-filter-themes ()
-  (setq circadian-themes '(("5:00"  . wombat)
-                           ("7:00"  . adwaita)
-                           ("9:00"  . wombat)
-                           ("11:00" . adwaita)
-                           ("12:00" . wombat)
-                           ("13:00" . adwaita)
-                           ("14:00" . tango)
-                           ("15:00" . adwaita)
-                           ("16:00" . wombat)
-                           ("17:00" . adwaita)
-                           ("18:00" . tango)
-                           ("19:00" . adwaita)
-                           ("21:00" . wombat)
-                           ("23:00" . adwaita)))
+  "Test filtering of `circadian-themes' list.`"
+  (setq circadian-themes '(("5:01"  . wombat)
+                           ("7:15"  . adwaita)
+                           ("9:30"  . wombat)
+                           ("11:31" . adwaita)
+                           ("12:35" . wombat)
+                           ("13:45" . adwaita)
+                           ("14:47" . tango)
+                           ("15:58" . adwaita)
+                           ("16:59" . wombat)
+                           ("17:02" . adwaita)
+                           ("18:09" . tango)
+                           ("19:54" . adwaita)
+                           ("21:12" . wombat)
+                           ("23:59" . adwaita)))
   (let ((time-now "4:10"))
     (should (equal 0 (length (circadian-filter-inactivate-themes
                               circadian-themes
@@ -43,7 +46,10 @@
                               circadian-themes
                               time-now))))))
 
+
+
 (ert-deftest test-circadian-activate-latest-theme ()
+  "Test `circadian-activate-latest-theme' used in `circadian-setup'."
   (setq circadian-themes '(("7:00" . wombat)
                            ("16:00" . tango)))
   (with-mock
@@ -54,6 +60,24 @@
    (stub circadian-now-time-string => "17:00")
    (circadian-activate-latest-theme)
    (should (equal 'tango (cl-first custom-enabled-themes)))))
+
+
+
+(ert-deftest test-circadian-sunrise-sunset ()
+  "Test :sunrise and :sunset keywords for theme switching."
+  (setq calendar-latitude 49.329896)
+  (setq calendar-longitude 8.570925)
+  (setq circadian-themes '((:sunrise . wombat)
+                           (:sunset . adwaita)))
+  (should (equal 1 (length (circadian-filter-inactivate-themes
+                            circadian-themes
+                            "12:00"))))
+  (should (equal 2 (length (circadian-filter-inactivate-themes
+                            circadian-themes
+                            "23:59")))))
+
+
+
 (ert-deftest test-circadian-time-comparisons ()
   "Test comparison of time strings.
    Time A: 17:59
